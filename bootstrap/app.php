b07a31b7642a688d5c016f30fd5e1bfe76fb3e9c
Omercyn 469 <omercyn496@gmail.com>
2025-10-06 19:29:15 +0700

    two com

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,13 +3,20 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\App;
 
+
+if (isset($_ENV['VERCEL_URL']) || env('APP_ENV') === 'production') {
+    // Arahkan storage ke direktori /tmp
+    $app->useStoragePath('/tmp/storage');
+}
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    
     ->withMiddleware(function (Middleware $middleware) {
         // ... middleware lain jika ada
         $middleware->trustProxies(at: '*'); // TAMBAHKAN INI
